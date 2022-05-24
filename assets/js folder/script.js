4b29872219c34bb044cfed59e751ee22ecd4550a
Lorreina Guyett <slorreina369@gmail.com>
2022-05-23 22:23:35 -0700

    form reset function added

--- assets/js folder/script.js
+++ assets/js folder/script.js
@@ -7,6 +7,12 @@ var TaskFormHandler = function(event){
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    if(!taskNameInput || !taskTypeInput){
+        alert("You need to fill out the task form!");
+        return false;
+    }
+    formEl.reset();
+
     var TaskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
