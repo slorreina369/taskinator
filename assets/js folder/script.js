a582586938ead0e6f752ac0dd74585c36be51852
Lorreina Guyett <slorreina369@gmail.com>
2022-05-23 18:29:53 -0700

    Form submit refractor

--- assets/js folder/script.js
+++ assets/js folder/script.js
@@ -1,21 +1,29 @@
 var formEl = document.querySelector('#task-form');
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event){
+var TaskFormHandler = function(event){
     
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-    
+
+    var TaskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+    createTaskEl(TaskDataObj);
+};
+var createTaskEl = function(TaskDataObj){
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
 
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
     
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + TaskDataObj.name + "</h3><span class='task-type'>" + TaskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
 
     tasksToDoEl.appendChild(listItemEl);
+
 };
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", TaskFormHandler);
