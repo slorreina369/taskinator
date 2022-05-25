0c78350ee919efc5a2cfbcbc92b9f8a7718d85f9
Lorreina Guyett <slorreina369@gmail.com>
2022-05-24 17:38:26 -0700

    delete and edit buttons established

--- assets/js folder/script.js
+++ assets/js folder/script.js
@@ -1,6 +1,7 @@
 var formEl = document.querySelector('#task-form');
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
+var pageContentEl = document.querySelector("#page-content");
 
 var TaskFormHandler = function(event){
     
@@ -76,4 +77,36 @@ var createTaskEl = function(TaskDataObj){
     tasksToDoEl.appendChild(listItemEl);
 
 };
+var deleteTask = function(taskId){
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+
+};
+var editTask = function(taskId){
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+    document.querySelector("input[name='task-name']").value = taskName;
+
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+    document.querySelector("select[name='task-type']").value = taskType;
+
+    document.querySelector("#save-task").textContent = "Save Task";
+    formEl.setAttribute("data-task-id", taskId);
+};
+var taskButtonHandler = function(event){
+    console.log(event.target);
+    var targetEl = event.target;
+
+    if(targetEl.matches(".edit-btn")){
+        var taskId = targetEl.getAttribute("data-task-id");
+        editTask(taskId);
+    } else if(targetEl.matches(".delete-btn")){
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+pageContentEl.addEventListener("click", taskButtonHandler);
+
 formEl.addEventListener("submit", TaskFormHandler);
